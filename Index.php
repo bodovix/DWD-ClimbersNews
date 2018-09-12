b458c99c581037fdca13aedbd3b5e5b947525b71
bodovix <stbbap@gmail.com>
2018-09-12 14:43:15 +0100

    project setup with bootstrap

---
+++ Index.php
@@ -0,0 +1,15 @@
+<html>
+<header>
+<?php include 'View/Header.php' ?>
+</header>
+<body class="">
+<button class="btn btn-danger">test</button>
+</body>
+<footer>
+<?php
+include 'View/Footer.php'
+?>
+</footer>
+</html>
+
+
---
+++ View/Header.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+          crossorigin="anonymous">
+</head>
+<div>
+    <div id="banner" class="col-12 bg-dark" style="height: 100px;">
+        <p class="align-middle text-danger h1" id="title">Climbers News</p>
+    </div>
+</div>
