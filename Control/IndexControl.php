1f93f84c90179a4184be503fd15611ba0a84c032
bodovix <stbbap@gmail.com>
2018-09-13 16:38:22 +0100

    db read successfull

--- Control/IndexControl.php
+++ Control/IndexControl.php
@@ -1,34 +1,40 @@
 <?php
 
-require_once "../global/dbConnect.php";
-
-
-
 
 class IndexControl
 {
     private $con;
-   public function LoadArticleCard($con){
-        $articlesToShow[] = $this->GetAllArticles($con);
-        foreach ($articlesToShow as $article ){
+    public function __construct($conection)
+    {
+        $this->con = $conection;
+    }
 
+    public function GetArticles(){
+        $query = $this->con-> prepare("select * from sql1701267.article ORDER BY createdOn DESC ");
+
+        $success = $query -> execute();
+        if ($success){
+            $result = $query -> fetchAll(PDO::FETCH_ASSOC);
+            $html ="";
+            foreach ($result as $item) {
+                $html .=
+                    <<<EOT
+
+        <div class="col-md-4 bg-info">
+            <div class="container mt-3">
+                <div class="card" >
+                    <img class="card-img-top img-fluid " src="{$item["imageUrl"]}" style="height: 200px;width: auto; overflow: hidden" >
+                    <div class="card-body">
+                        <h4 class="card-title my-1">{$item["headline"]}"</h4>
+                        <p class="card-text mb-1">{$item["description"]}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+EOT;
+            }
+            return $html;
         }
-   }
-
-   private  function GetAllArticles($con)
-   {
-       $query = $con->prepare("select * from sql1701267.article ORDER BY createdOn DESC ");
-
-       $success = $query->execute([]);
-       if ($success) {
-            $result = $query->fetchAll(PDO::FETCH_ASSOC);
-       }
-       return $result;
-
-   }
-
-   public function __construct($conection)
-   {
-       $this->con = $conection;
-   }
+    }
 }
+
