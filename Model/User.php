87fd1945b04bcd4a0cb9d2f104206dec59efbd84
Gwydion <stbbap@gmail.com>
2018-10-01 12:48:03 +0100

    added User model class

---
+++ Model/User.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Gwydion
+ * Date: 10/1/2018
+ * Time: 12:46 PM
+ */
+class User
+{
+    //TODO: Get User
+
+    //TODO: Create USer
+
+    //TODO: Update User
+
+    //TODO: delete User
+}
