42b83c49f8b7fc183c375911dd40bf3b22479a57
Bodovix <stbbap@gmail.com>
2018-10-25 11:57:13 +0100

    updated Week 1 Evaluation after feedback week

--- View/Includes/Evaluations/Evaluation-Week-1.php
+++ View/Includes/Evaluations/Evaluation-Week-1.php
@@ -1,7 +1,3 @@
-<?php
-require_once 'config/config.php';
-?>
-
 
 <div class="container">
     <div class="container">
@@ -9,16 +5,18 @@ require_once 'config/config.php';
     </div>
 
     <h2>HTML frameworks – comparison of grid systems</h2>
-    <p class="text-justify">The Two HTML Frameworks I considered for this project were Bootstrap and Pure.CSS. Bootstrap is an Open Source originally created by Twitter and Pure.CSS is another OS Framework by Yahoo.
+    <p class="text-justify">There are many different HTML frameworks available to develop a website against such as Bootstrap and Pure.CSS. Bootstrap is an Open Source originally created by Twitter and Pure.CSS is another OS Framework by Yahoo.
         Both frameworks work under a grid system. Grid systems are a way of splitting the page into rows and columns to allow for quick setup of mobile responsive websites.
         Bootstrap and Pure handle their grid systems differently. Bootstrap uses a 12 Column grid meaning the narrowest fraction of the screen a division can take up is 1/12th of the screen whereas Pure uses a more granular 1/5th or 1/24th grid.
-        I chose bootstrap as working in 1/12th’s was the best compromise between functionality and usability. Whereas Pure.CSS’s 1/5th would be too ridged and 1/24th’s would be unnecessarily complicated.
+        Bootstrap was implemented in the site as working in 1/12th’s was the best compromise between functionality and usability. Whereas Pure.CSS’s 1/5th would be too ridged and 1/24th’s would be unnecessarily complicated.
     </p>
 
     <h2>Bootstrap menu system</h2>
     <p class="text-justify">Developing multi-layer menus from scratch is surprisingly complicated and almost guaranteed to waste hours of development time. Bootstrap avoids this pitfall by making use of its readymade responsive styles that have been preconfigured to be fully mobile responsive and allow for easy implementations of menus.	</p>
-    <img src="uploads/Evaluations/Week-1/Bootstrap-dropdown-Nav.png">
-    <p class="text-justify">One negative that I have taken from bootstraps navigation menus is whilst fast to setup they can look quite similar as native bootstrap only provides a limited number of options for colour. Additional Styling would be required after functional development. </p>
+    <div class="container text-center">
+        <img src="uploads/Evaluations/Week-1/Bootstrap-dropdown-Nav.png">
+    </div>
+    <p class="text-justify">The downside of Bootstraps quick and easy navigation menus is, whilst fast to setup they can look quite similar as native bootstrap only provides a limited number of options for colour. Additional Styling will be required after functional development to keep the website unique. </p>
 
     <h2></h2>
     <p class="text-justify"></p>
@@ -30,8 +28,18 @@ require_once 'config/config.php';
         Bootstrap provides us with the concept of Cards. These are basically responsive Tile icons that can be customized to display the websites content.
         Another useful feature of bootstrap is its Alerts. Bootstrap has 8 standard alert styles to fit any return message scenario the user might face whilst visiting our website.
     </p>
-    <img src="uploads/Evaluations/Week-1/Bootstrap-Alerts.png">
-    <p>Bootstrap offers developers the ability to create professional dynamic progress bars that could be used to indicate the progress through any number of tasks the user may have to carry out or the state of any Ajax calls the website is making. Unfortunately, as with the pagination these progress bars must be manually powered by the developer’s, bootstrap is predominantly a CSS framework.</p>
+    <div class="container text-center">
+        <img src="uploads/Evaluations/Week-1/Bootstrap-Alerts.png">
+    </div>
+    <br>
+    <br>
+    <p>Bootstrap offers developers the ability to create professional dynamic progress bars that could be used to indicate the progress through any number of tasks the user may have to carry out or the state of any Ajax calls the website is making. Unfortunately, as with the pagination these progress bars must be manually powered by the developer’s using JavaScript or another language as seen in the Register form, bootstrap is predominantly a CSS framework.</p>
+
+    <div class="container text-center">
+        <img src="uploads/Evaluations/Week-1/passwordStrength.JPG"/>
+    </div>
+    </br>
+    </br>
 
 </div>
 </html>
