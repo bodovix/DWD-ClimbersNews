f6a31172cde1dfd100a365456c440c2ce751ffeb
Bodovix <stbbap@gmail.com>
2018-11-12 23:15:18 +0000

    started re-working iot2

--- Controller/ElectricImpControl.php
+++ Controller/ElectricImpControl.php
@@ -38,6 +38,38 @@ EOT;
             return null;
         }
     }
+    public function DisplayRecentReadingsWithDropdown(){
+        //get readings
+        $readingsJson = $this->impModel->GetMostRecentReadings();
+        $readings = json_decode($readingsJson);
+
+
+        if (count($readings) != 0){
+            //readings found, Display them
+            $html = "";
+            foreach($readings as $record){
+                $data = (object) [
+                    'id' => $record->id,
+                    'createdOn' =>  $record->createdOn,
+                    'data' => json_decode($record->dataJson)
+                ];
+
+                $html .= <<<EOT
+                <div data-role="collapsible" id="recentReadingsUL">
+                    <h1>{$data->createdOn}</h1>
+                        <p>Device:   {$data->data->device}</p>
+                        <p>External:   {$data->data->external}</p>
+                        <p>Light Level:   {$data->data->lightLevel}</p>
+                        <p>Internal:   {$data->data->internal}</p>
+                        <p>Voltage:   {$data->data->voltage}</p>
+                </div>
+EOT;
+            }
+            return $html;
+        }else{
+            return null;
+        }
+    }
     public function GetResultByID($id){
         $json = $this->impModel->GetReadingByID($id);
         $result = json_decode($json);
